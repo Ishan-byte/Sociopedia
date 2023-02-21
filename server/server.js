12c721e810e417e0cdddadec314e024838735cb5
Ishan Chemjong <limbuisan@gmail.com>
2023-02-21 23:02:14 +0545

    Application Middleware Setup

---
+++ server/server.js
@@ -0,0 +1,39 @@
+import express from "express";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
+import bodyParser from "body-parser";
+import { fileURLToPath } from "url";
+import cors from "cors";
+import path from "path";
+import multer from "multer";
+
+// CONFIGURATIONS
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json({ limit: "30mb" }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+app.use("/static", express.static(path.join(__dirname, "public/assets")));
+
+// FILE STORAGE
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/assets");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
+app.listen(5000, () => {
+  console.log("Server is running");
+});
