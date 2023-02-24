a6d4032bed231db32636e51c00db7644c3577378
Ishan Chemjong <limbuisan@gmail.com>
2023-02-24 09:30:42 +0545

    Theme and Styling Setup Completion

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,32 @@
+import { CssBaseline } from "@mui/material";
+import { createTheme, ThemeProvider } from "@mui/system";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 // Components
 import Home from "scenes/homePage";
 import Login from "scenes/loginPage";
 import Profile from "scenes/profilePage";
+import { themeSettings } from "theme";
 
 function App() {
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => {
+    createTheme(themeSettings(mode));
+  }, [mode]);
+
   return (
     <div className="app">
       <Router>
-        <Routes>
-          <Route path="/" exact element={<Home />}></Route>
-          <Route path="/login" exact element={<Login />}></Route>
-          <Route path="/profile/:userId" exact element={<Profile />}></Route>
-        </Routes>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route path="/" exact element={<Home />}></Route>
+            <Route path="/login" exact element={<Login />}></Route>
+            <Route path="/profile/:userId" exact element={<Profile />}></Route>
+          </Routes>
+        </ThemeProvider>
       </Router>
     </div>
   );
